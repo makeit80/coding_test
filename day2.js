68b0dbf3d9b4fe1da6d3592f6b60c44dcca9d86d
makeit80 <lkwdevelop@gmail.com>
2023-10-20 10:36:41 +0900

    2023.10.20. 분수의 덧셈

--- day2.js
+++ day2.js
@@ -1,43 +1,27 @@
 // 2023.10.19.
+//첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1,
+//두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가
+//매개변수로 주어집니다.
+//두 분수를 더한 값을 기약 분수로 나타냈을 때
+//분자와 분모를 순서대로 담은 배열을 return 하도록
+//solution 함수를 완성해보세요.
 
-numer1 = 1;
-donom1 = 2;
-numer2 = 3;
+numer1 = 4;
+donom1 = 4;
+numer2 = 4;
 donom2 = 4;
 
-function solution(numer1, denom1, numer2, denom2) {
-  var answer = [];
-  var arr1 = [];
-  var arr2 = [];
-  let a = [];
+function solution2(denum1, num1, denum2, num2) {
+  let topNum = num1*denum2 + num2*denum1
+  let botNum = num1*num2
+  let maximum = 1
 
-  for (i = 1; a.length < 1; i++) {
-    arr1.push(denom1 * i);
-    arr2.push(denom2 * i);
-    a = arr1.filter(x => arr2.includes(x))
+  for(let i = 1 ; i <= topNum ; i ++) {
+      if(topNum%i === 0 && botNum%i === 0) {
+          maximum = i
+      }
   }
-
-  let arr = [arr1, arr2];
-  let numer = [numer1, numer2];
-  let b = 0;
-
-  for (i = 0; i < arr.length; i++) {
-    n = index(arr[i], a[0], numer[i])
-    console.log('n' + n)
-    b += n;
-  }
-
-  answer.push(b, a[0])
-  return answer;
+  return [topNum/maximum, botNum/maximum]
 }
 
-
-function index(arr, a, numer) {
-  let index = arr.indexOf(a) + 1;
-  if (index > 0) {
-    numer *= index;
-  }
-  return numer
-}
-
-console.log(solution(numer1, donom1, numer2, donom2));
+console.log(solution2(numer1, donom1, numer2, donom2));
